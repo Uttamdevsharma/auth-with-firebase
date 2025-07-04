67441852392e78102d0592787b8afd8bb0e5929a
Uttam <uttamrohit4545@gmail.com>
2025-07-04 15:29:14 +0600

    fix:dashboard responsive

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -36,14 +36,14 @@ const DashBoard = () => {
         <main class="flex-1 bg-gray-100 p-6">
             <h1 class="text-2xl font-semibold mb-4">Main Content</h1>
 
-            <p class="text-gray-700">Lorem ipsum dolor sit amet consectetur 
+            <p class="text-gray-500">Lorem ipsum dolor sit amet consectetur 
                 adipisicing elit. Laboriosam voluptate commodi maiores 
                 odit adipisci explicabo, recusandae quas, facilis ratione, 
                 dolor cumque. Necessitatibus molestiae soluta rerum vel facilis illo perspiciatis voluptas iure, fuga nemo. Sunt, velit. Enim eos ratione perspiciatis corrupti exercitationem? Voluptatibus dolorem tenetur odio ad distinctio quo nostrum libero fugit obcaecati cupiditate fuga deserunt, 
                 alias, laborum velit quia voluptas.</p>
 
                 <div class="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4">
-                    
+
                     <div class="bg-white p-4 shadow rounded">Box1</div>
                     <div class="bg-white p-4 shadow rounded">Box2</div>
                 </div>
